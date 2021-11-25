8d10c052113e48c98ae75efa9b85603edc2e814e
RChaubey16 <ruturajchaubey16@gmail.com>
2021-11-25 20:08:34 +0530

    Added the state to the project via the useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
 
+  // useState hook : Used to change any variable or content of the site.
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+
   const clickHandler = () => {
-    console.log("Clicked!!!!!");
+    // Will take the new value, assign it to title state variable and it will re-call or re-execute this component function to re-render the JSX of this component
+    setTitle("Updated!");
+    setAmount("Rs 2000");
+    console.log(title);
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date = {props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">$ {props.amount} </div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">$ {amount} </div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
