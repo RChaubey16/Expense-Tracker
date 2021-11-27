c7bebe52a93cf06bdd97f406fbc31a12943ab13e
RChaubey16 <ruturajchaubey16@gmail.com>
2021-11-27 10:49:36 +0530

    Added a form to input expense

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-
+import React from 'react';
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -26,10 +27,8 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Lets Get Started!</h1>
-      <div>
-        <Expenses items = {expenses} />
-      </div>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" />
+        </div>
+
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="01-01-2021" max="31-12-2023" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+          <button type="submit">Add</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
