e955fda7917c83c28b863e73c3637e896db13b61
RChaubey16 <ruturajchaubey16@gmail.com>
2021-11-30 19:21:56 +0530

    Followed the previous state approach in one single state management

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,52 +2,65 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-
   // Method 1 : Multiple slices of state
-  // const [enteredTitle, setEnteredTitle] = useState('');
-  // const [enteredAmount, setEnteredAmount] = useState('');
-  // const [enteredDate, setEnteredDate] = useState('');
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
   // Method 2 : one single state for storing all the 3 values.
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: ''
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
-    const titleChangeHandler = (event) => {
-      // Method 1 :
-      // setEnteredTitle(event.target.value);
+  const titleChangeHandler = (event) => {
+    // Method 1 :
+    setEnteredTitle(event.target.value);
 
-      // Method 2 : 
-      setUserInput({
-        ...userInput,
-        enteredTitle: event.target.value
-      });
+    // Method 2 A: Using one state:
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value
+    // });
 
-    }
+    // Method 2 B: Using one state (the correct approach)
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value }; // New state snapshot
+    // });
+  };
 
-    const amountChangeHandler = event => {
-       // Method 1 :
-      // setEnteredTitle(event.target.value);
+  const amountChangeHandler = (event) => {
+    // Method 1 :
+    setEnteredTitle(event.target.value);
 
-      // Method 2 : 
-      setUserInput({
-        ...userInput,
-        enteredAmount: event.target.value,
-      });
-    }
+    // Method 2 A: Using one state
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
 
-    const dateChangeHandler = event => {
-       // Method 1 :
-      // setEnteredTitle(event.target.value);
+    // Method 2 B: Using one state (the correct approach, dependant on previous state)
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: event.target.value};  // New state snapshot
+    // });
+  };
 
-      // Method 2 : 
-      setUserInput({
-        ...userInput,
-        enteredDate: event.target.value,
-      })
-    }
+  const dateChangeHandler = (event) => {
+    // Method 1 :
+    setEnteredTitle(event.target.value);
+
+    // Method 2 A: Using one state
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+
+    // Method 2 B: Using one state (the correct approach)
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: event.target.value };  // New state snapshot
+    // })
+  };
 
   return (
     <form>
@@ -55,21 +68,31 @@ const ExpenseForm = () => {
         <div className="new-expense__control">
           <label>Title</label>
           <input type="text" onChange={titleChangeHandler} />
-          <h2>{enteredAmount}</h2>
+          <h2>{enteredTitle}</h2>
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="01-01-2021" max="31-12-2023" onChange={dateChangeHandler} />
+          <input
+            type="date"
+            min="01-01-2021"
+            max="31-12-2023"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
-          <button type="submit">Add</button>
+        <button type="submit">Add</button>
       </div>
     </form>
   );
