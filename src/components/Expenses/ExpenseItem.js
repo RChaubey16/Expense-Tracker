8bb2cb340b43c1f9e1317148fea62bd562eff79a
RChaubey16 <ruturajchaubey16@gmail.com>
2021-12-13 18:45:31 +0530

    Updated the expense-item component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,24 +6,23 @@ import "./ExpenseItem.css";
 function ExpenseItem(props) {
 
   // useState hook : Used to change any variable or content of the site.
-  const [title, setTitle] = useState(props.title);
-  const [amount, setAmount] = useState(props.amount);
+  // const [title, setTitle] = useState(props.title);
+  // const [amount, setAmount] = useState(props.amount);
 
-  const clickHandler = () => {
-    // Will take the new value, assign it to title state variable and it will re-call or re-execute this component function to re-render the JSX of this component
-    setTitle("Updated!");
-    setAmount("Rs 2000");
-    console.log(title);
-  }
+  // const clickHandler = () => {
+  //   // Will take the new value, assign it to title state variable and it will re-call or re-execute this component function to re-render the JSX of this component
+  //   setTitle("Updated!");
+  //   setAmount("Rs 2000");
+  //   console.log(title);
+  // }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date = {props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">$ {amount} </div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount} </div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
